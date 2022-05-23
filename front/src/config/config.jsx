4405dbb6fe51c1b03c17f9a64a442d57ebca1bad
Jesús DIaz Rivas <jesus20.11@hotmail.es>
2022-05-23 20:45:27 +0200

    [FIX] env

--- front/src/config/config.jsx
+++ front/src/config/config.jsx
@@ -1,6 +1,6 @@
 export const cfg = {
   env: process.env.NODE_ENV || 'development',
-  apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:8080/api/',
+  apiUrl: process.env.REACT_APP_API_URL || 'https://coinholder.tech/',
   coingeckoAPI: process.env.COINGECKO_API_URL || 'https://api.coingecko.com/api/v3/',
   captcha:{
     site: process.env.RECAPTCHA_SITE_KEY || '6LdwS-EfAAAAADO2PW7BONXKd9x_c0LbQ-fOmytG'
