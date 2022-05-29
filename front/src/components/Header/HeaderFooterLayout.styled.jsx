bb1190c72130238a0f39a42dfc173786d162379f
Jesús DIaz Rivas <jesus20.11@hotmail.es>
2022-05-29 20:45:29 +0200

    [FIX] navbar responsive difficult for users to expand

--- front/src/components/Header/HeaderFooterLayout.styled.jsx
+++ front/src/components/Header/HeaderFooterLayout.styled.jsx
@@ -14,12 +14,22 @@ export const StyledHeader = styled(Header)`
   border-bottom: 2px solid rgba(0, 0, 0, 0.06);
   font-family: Roboto, sans-serif;
   box-shadow: 0 1px 10px rgb(151 164 175 / 10%);
+  padding: 24px 5px !important;
   #management {
     display: flex;
     flex-direction: row;
     align-items: center;
+    flex-wrap: wrap;
+    div:last-child {
+      margin: auto;
+    }
+    @media screen and (min-width: 500px) {
+          flex-wrap: nowrap;
+    }
   }
   img {
     display: block;
   }
+  height: auto ;
+
 `;
--- front/src/components/Nav/Nav.styled.jsx
+++ front/src/components/Nav/Nav.styled.jsx
@@ -9,7 +9,21 @@ export const StyledNav = styled(Menu)`
   min-width: 12%;
   justify-content: center;
   border-bottom: none;
-  .ant-menu-vertical{
+  .ant-menu-vertical {
     padding: 10px;
+  }
+
+  .ant-menu-submenu {
+    text-align: center;
+    padding: 0px !important;
+    width: 100%;
+    
+    svg {
+      font-size: 1.5rem;
+      vertical-align: middle;
+    }
+    @media screen and (min-width: 485px) {
+      width: 25px;
+    }
   }
 `;
